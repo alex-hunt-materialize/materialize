dffcdfc203a028bd20153effaebf8e0f35bfb9f4
Alex Hunt <alex.hunt@materialize.com>
2026-02-11 13:29:41 +0000

    capture_output instead of pipes

--- test/orchestratord/mzcompose.py
+++ test/orchestratord/mzcompose.py
@@ -2765,8 +2765,7 @@ def run(definition: dict[str, Any], expect_fail: bool) -> None:
         result = subprocess.run(
             ["kubectl", "apply", "-f", "-"],
             input=yaml_str.encode(),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            capture_output=True,
         )
         if result.returncode == 0:
             break
